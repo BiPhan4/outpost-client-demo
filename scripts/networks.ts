aedfbc6abde07d98ba55f163d7f4eebd0aa6a102
BiPhan4 <biphan374@gmail.com>
2024-06-18 13:36:55 -0400

    add archway testnet config

--- scripts/networks.ts
+++ scripts/networks.ts
@@ -19,3 +19,13 @@ export const wasmdConfig: Network = {
   // Haven't set up a faucet URL for wasmd yet, we're just funding the account in the e2e environment
   faucetUrl: "https://faucet.malaga-420.cosmwasm.com/",
 };
+
+export const archwayTestnetConfig: Network = {
+  chainId: "constantine-3",
+  rpcEndpoint: "http://172.233.221.58:26657", // This is our personal RPC 
+  prefix: "archway",
+  gasPrice: GasPrice.fromString("1000000000000000aconst"),
+  feeToken: "aconst",
+  // TODO: set up faucet
+  faucetUrl: "",
+};
